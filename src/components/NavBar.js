4b99c26b8c534d6d026ee9f1ece9613beef17d03
egesua <egesu06@gmail.com>
2024-03-29 16:25:54 +0300

    scroll event listener and useEffect.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,25 @@
 import React from "react";
 import { Navbar, Container } from "react-bootstrap";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const NavBar = () => {
 
     const { activeLink, setActiveLink } = useState('home');
+    const { scrolled, setSetrolled } = useState(false);
+
+    useEffect(() => {
+        const onScroll = () => {
+            if( window.scrollY > 50 ) {
+                setSetrolled(true);
+            } else {
+                setSetrolled(false);
+            }
+        }
+
+        window.addEventListener("scroll", onScroll);
+
+        return () => window.removeEventListener("scroll", onScroll)
+    }, []);
 
   return (
     <div>
