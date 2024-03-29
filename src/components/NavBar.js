c8ced9b7348a72170931890d1ba2413e03d64d76
egesua <egesu06@gmail.com>
2024-03-29 16:28:45 +0300

    conditional rendering.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,27 +3,26 @@ import { Navbar, Container } from "react-bootstrap";
 import { useState, useEffect } from "react";
 
 const NavBar = () => {
+  const { activeLink, setActiveLink } = useState("home");
+  const { scrolled, setSetrolled } = useState(false);
 
-    const { activeLink, setActiveLink } = useState('home');
-    const { scrolled, setSetrolled } = useState(false);
+  useEffect(() => {
+    const onScroll = () => {
+      if (window.scrollY > 50) {
+        setSetrolled(true);
+      } else {
+        setSetrolled(false);
+      }
+    };
 
-    useEffect(() => {
-        const onScroll = () => {
-            if( window.scrollY > 50 ) {
-                setSetrolled(true);
-            } else {
-                setSetrolled(false);
-            }
-        }
+    window.addEventListener("scroll", onScroll);
 
-        window.addEventListener("scroll", onScroll);
-
-        return () => window.removeEventListener("scroll", onScroll)
-    }, []);
+    return () => window.removeEventListener("scroll", onScroll);
+  }, []);
 
   return (
     <div>
-      <Navbar expand="lg">
+      <Navbar expand="lg" className={scrolled ? "scrolled" : ""}>
         <Container>
           <Navbar.Brand href="#home">
             <img src={""} alt="Logo" />
@@ -33,9 +32,32 @@ const NavBar = () => {
           </Navbar.Toggle>
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#skills">Link</Nav.Link>
-              <Nav.Link href="#projects">Projects</Nav.Link>
+              <Nav.Link
+                href="#home"
+                className={
+                  activeLink === "home" ? "active navbar-link" : "navbar-link"
+                }
+              >
+                Home
+              </Nav.Link>
+              <Nav.Link
+                href="#skills"
+                className={
+                  activeLink === "skills" ? "active navbar-link" : "navbar-link"
+                }
+              >
+                Link
+              </Nav.Link>
+              <Nav.Link
+                href="#projects"
+                className={
+                  activeLink === "projects"
+                    ? "active navbar-link"
+                    : "navbar-link"
+                }
+              >
+                Projects
+              </Nav.Link>
             </Nav>
             <span className="navbar-text">
               <div className="social-icon">
