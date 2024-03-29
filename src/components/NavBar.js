02ef9c70a009ed6a9ef5407dc684863bbaf6615d
egesua <egesu06@gmail.com>
2024-03-29 16:49:40 +0300

    custom fonts added. styling to be done.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,10 @@
 import React from "react";
 import { Navbar, Container } from "react-bootstrap";
 import { useState, useEffect } from "react";
+import logo from "../assets/images/logo.svg";
+import navIcon1 from "../assets/images/nav-icon1.svg";
+import navIcon2 from "../assets/images/nav-icon2.svg";
+import navIcon3 from "../assets/images/nav-icon3.svg";
 
 const NavBar = () => {
   const { activeLink, setActiveLink } = useState("home");
@@ -22,14 +26,14 @@ const NavBar = () => {
 
   const onUpdateActiveLink = (value) => {
     setActiveLink(value);
-  }
+  };
 
   return (
     <div>
       <Navbar expand="lg" className={scrolled ? "scrolled" : ""}>
         <Container>
           <Navbar.Brand href="#home">
-            <img src={""} alt="Logo" />
+            <img src={logo} alt="Logo" />
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav">
             <span className="navbar-toggler-icon"></span>
@@ -69,13 +73,13 @@ const NavBar = () => {
             <span className="navbar-text">
               <div className="social-icon">
                 <a href="#">
-                  <img src={""} alt="" />
+                  <img src={navIcon1} alt="" />
                 </a>
                 <a href="#">
-                  <img src={""} alt="" />
+                  <img src={navIcon2} alt="" />
                 </a>
                 <a href="#">
-                  <img src={""} alt="" />
+                  <img src={navIcon3} alt="" />
                 </a>
               </div>
               <button className="vvd" onClick={() => console.log("connect")}>
