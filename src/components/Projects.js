a45141a19a0d7904b4007f586f31381768c9263a
egesua <egesu06@gmail.com>
2024-03-30 16:11:46 +0300

    added to the app.js and added dummy projects for to be completed.

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,40 @@
 import React from 'react'
 
 const Projects = () => {
+
+    const projects = [
+        {
+            title: "E-Commerce Website",
+            description: "Design & Development",
+            imgUrl: projImg1,
+        },
+        {
+            title: "E-Commerce Website",
+            description: "Design & Development",
+            imgUrl: projImg1,
+        },
+        {
+            title: "E-Commerce Website",
+            description: "Design & Development",
+            imgUrl: projImg1,
+        },
+        {
+            title: "E-Commerce Website",
+            description: "Design & Development",
+            imgUrl: projImg1,
+        },
+        {
+            title: "E-Commerce Website",
+            description: "Design & Development",
+            imgUrl: projImg1,
+        },
+        {
+            title: "E-Commerce Website",
+            description: "Design & Development",
+            imgUrl: projImg1,
+        },
+    ]
+
   return (
     <div>
       
