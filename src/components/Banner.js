38b01439e0e7a7111c9e1d3e737732ca8ebf046e
egesua <egesu06@gmail.com>
2024-03-30 15:44:54 +0300

    banner styling

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -51,8 +51,8 @@ const Banner = () => {
           <Col xs={12} md={6} xl={7}>
             <span className="tagline">Welcome to my Portfolio</span>
             <h1>
-              {`Hi I'm Ege`}
-              <span className="wrap">Full Stack Developer</span>
+              {`Hi I'm Ege `}
+              <span className="wrap">{text}</span>
             </h1>
             <p>
               Lorem ipsum dolor sit amet. Simply a dummy text of the printing
