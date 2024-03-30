a9c21025afbfcf9ada7c94398c1a996792d86275
egesua <egesu06@gmail.com>
2024-03-30 16:07:22 +0300

    skills component completed(for now) and added to the app.js

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,11 +1,67 @@
-import React from 'react'
+import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import meter1 from "../assets/images/meter1.svg"
+import meter2 from "../assets/images/meter2.svg"
+import meter3 from "../assets/images/meter3.svg"
+import colorSharp from "../assets/images/color-sharp.png"
 
 const Skills = () => {
+  const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5,
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+    },
+  };
+
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <section className="skill" id="skills">
+      <Container>
+        <Row>
+          <Col>
+            <div className="skill-bx">
+              <h2>Skills</h2>
+              <p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s, when an unknown printer took a galley
+                of type and scrambled it to make a type specimen book.
+              </p>
+              <Carousel responsive={responsive} infinite={true} className="skill-slider">
+                <div className="item">
+                    <img src={meter1} alt="Image" />
+                    <h5>Full Stack Development</h5>
+                </div>
+                <div className="item">
+                    <img src={meter2} alt="Image" />
+                    <h5>Web Development</h5>
+                </div>
+                <div className="item">
+                    <img src={meter3} alt="Image" />
+                    <h5>Backend Development</h5>
+                </div>
+              </Carousel>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+      <img className="background-image-left" src={colorSharp} alt="leftImage" />
+    </section>
+  );
+};
 
 export default Skills;
