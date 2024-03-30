8bcd91ed4933dd4326e5aff52ff7c13548f2c6a6
egesua <egesu06@gmail.com>
2024-03-30 15:39:48 +0300

    banner component logic part upgrades.

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,28 +1,74 @@
-import React from 'react'
-import { Col, Container, Row } from 'react-bootstrap';
-import { ArrowRightCircle } from 'react-bootstrap-icons';
-import headerImg from '../assets/images/header-img.svg';
+import React, { useEffect, useState } from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import { ArrowRightCircle } from "react-bootstrap-icons";
+import headerImg from "../assets/images/header-img.svg";
 
 const Banner = () => {
+  const [loopNum, setLoopNum] = useState(0);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const toRotate = ["Full Stack Developer", "Web Designer", "UI/UX Designer"];
+  const [text, setText] = useState("");
+  const [delta, setDelta] = useState(300 - Math.random() * 100);
+  const period = 2000;
+
+  useEffect(() => {
+    let ticker = setInterval(() => {
+      tick();
+    }, delta);
+
+    return () => {
+      clearInterval(ticker);
+    };
+  }, [text]);
+
+  const tick = () => {
+    let i = loopNum % toRotate.length;
+    let fullText = toRotate[i];
+    let updatedText = isDeleting
+      ? fullText.substring(0, text.length - 1)
+      : fullText.substring(0, text.length + 1);
+
+    setText(updatedText);
+
+    if (isDeleting) {
+      setDelta((prevDelta) => prevDelta / 2);
+    }
+
+    if (!isDeleting && updatedText === fullText) {
+      setIsDeleting(true);
+      setDelta(period);
+    } else if (isDeleting && updatedText === "") {
+      setIsDeleting(false);
+      setLoopNum(loopNum + 1);
+      setDelta(500);
+    }
+  };
+
   return (
-    <section className='banner' id='home'>
+    <section className="banner" id="home">
       <Container>
-        <Row className='align-items-center'>
-            <Col xs={12} md={6} xl={7}>
-                <span className='tagline'>
-                    Welcome to my Portfolio
-                </span>
-                <h1>{`Hi I'm Ege`}<span className='wrap'>Full Stack Developer</span></h1>
-                <p>Lorem ipsum dolor sit amet. Simply a dummy text of the printing dummy texts.</p>
-                <button onCanPlay={() => console.log("connect")}>Let's connect. <ArrowRightCircle size={25} /></button>
-            </Col>
-            <Col xs={12} md={6} xl={5}>
-                <img src={headerImg} alt='Header Img' />
-            </Col>
+        <Row className="align-items-center">
+          <Col xs={12} md={6} xl={7}>
+            <span className="tagline">Welcome to my Portfolio</span>
+            <h1>
+              {`Hi I'm Ege`}
+              <span className="wrap">Full Stack Developer</span>
+            </h1>
+            <p>
+              Lorem ipsum dolor sit amet. Simply a dummy text of the printing
+              dummy texts.
+            </p>
+            <button onCanPlay={() => console.log("connect")}>
+              Let's connect. <ArrowRightCircle size={25} />
+            </button>
+          </Col>
+          <Col xs={12} md={6} xl={5}>
+            <img src={headerImg} alt="Header Img" />
+          </Col>
         </Row>
       </Container>
     </section>
-  )
-}
+  );
+};
 
 export default Banner;
