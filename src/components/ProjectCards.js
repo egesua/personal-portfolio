f34053c27efe542965dfc098df5d67a0cddddfb5
egesua <egesu06@gmail.com>
2024-03-31 11:29:34 +0300

    projectCards completed(for now)

--- src/components/ProjectCards.js
+++ src/components/ProjectCards.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import { Col } from 'react-bootstrap';
 
-const ProjectCards = () => {
+const ProjectCards = ({ title, description, imgUrl }) => {
   return (
-    <div>
-      
-    </div>
+    <Col sm={6} md={4}>
+      <div className='proj-imgbx'>
+        <img src={imgUrl} />
+        <div className='proj-txtx'>
+            <h4>{title}</h4>
+            <span>{description}</span>
+        </div>
+      </div>
+    </Col>
   )
 }
 
