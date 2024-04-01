301a36067004fb6a6b291a809073311efcc7cac7
egesua <egesu06@gmail.com>
2024-04-01 17:43:49 +0300

    handleSubmit completed(for now)

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -22,9 +22,25 @@ const Contact = () => {
     });
   };
 
-  const handleSubmit = () => {
-    
-  }
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setButtonText("Sending...");
+    let response = await fetch("http://localhost:3000/contact", {
+      method: "POST",
+      headers: {
+        "Content-Type": "Application/json;charset=utf-8",
+      },
+      body: JSON.stringify(formDetails),
+    });
+    setButtonText("Send");
+    let result = response.json();
+    setFormDetails(formInitialDetails);
+    if (result.code === 200) {
+      setStatus({ success: true, message: "Message sent succesfully." });
+    } else {
+      setStatus({ success: false, message: "Something went wrong... Try again later."})
+    }
+  };
 
   return (
     <section className="contact" id="connect">
