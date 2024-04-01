e6635801554124aa3c5e6a4aa57768014cd91f85
egesua <egesu06@gmail.com>
2024-04-01 17:51:44 +0300

    footer styling will be added. added to app.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Banner from "./components/Banner";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Banner />
       <Skills />
       <Projects />
+      <Footer />
     </div>
   );
 }
